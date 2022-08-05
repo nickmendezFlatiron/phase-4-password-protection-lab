e2333d0740955587612d8dc09cbef8cf39be73b0
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-08-05 14:03:27 -0400

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ #User Controller
+  post '/signup' , to: "users#create"
+  get '/me' , to: "users#show"
+
+  #Session Controller
+  post '/login' , to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+
 end
